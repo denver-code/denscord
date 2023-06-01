2e5e9a6746a7fe2dab5332ac4e4c5f61d673c193
denver-code <csigorek@gmail.com>
2023-06-01 15:49:17 +0100

    Added password generate func util

--- app/core/password.py
+++ app/core/password.py
@@ -7,3 +7,8 @@ def hash_password(password: str) -> str:
     """Returns a salted password hash"""
     
     return bcrypt.hashpw(password.encode(), settings.SALT_SECRET_KEY.encode()).decode()
+
+def generate_password(length: int = 8) -> str:
+    """Returns a random string of length"""
+    
+    return bcrypt.gensalt(length).decode()
