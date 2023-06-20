7334c5b0d933328ccd75af197242595a317a09d3
denver-code <csigorek@gmail.com>
2023-06-20 23:41:38 +0100

    Bulk users profiles search

--- api/public/user.py
+++ api/public/user.py
@@ -1,12 +1,26 @@
 from bson import ObjectId
 from fastapi import APIRouter, HTTPException
 
-from api.models.user import User, UserOut
+from api.models.user import BulkUsers, User, UserOut
 
 users_router = APIRouter(prefix="/profile")
 
+@users_router.get("/bulk")
 @users_router.get("/{id}")
-async def get_user_profile(id: str):
+async def get_user_profile(id: str, users: BulkUsers = None):
+    if users:
+        print(users.users)
+        _users = await User.find({"_id": {"$in": [ObjectId(user) for user in users.users]}}).to_list(1_000_000_000)
+        _users_list = []
+        for _user in _users:
+            _user = _user.dict()
+            _user["id"] = str(_user["id"])
+            _user = UserOut(**_user).dict()
+            del _user["email"]
+            _users_list.append(_user)
+        
+        return _users_list
+
     if not ObjectId.is_valid(id):
         raise HTTPException(status_code=400, detail="Invalid ID")
     
