45564b93c93cbd3fb12c521fd6bca7a775223b0c
denver-code <csigorek@gmail.com>
2023-06-20 23:07:15 +0100

    /profile/username route are now unavailable

--- api/private/user/profile.py
+++ api/private/user/profile.py
@@ -21,21 +21,21 @@ async def get_profile(request: Request):
     return user
 
 
-@profile_router.post("/username")
-async def set_username(request: Request, new_username: SetUsername):
-    if await User.find_one({"username": new_username.username}):
-        raise HTTPException(status_code=400, detail="Username already registered")
+# @profile_router.post("/username")
+# async def set_username(request: Request, new_username: SetUsername):
+#     if await User.find_one({"username": new_username.username}):
+#         raise HTTPException(status_code=400, detail="Username already registered")
     
-    auth_token = await FastJWT().decode(request.headers["Authorisation"])
+#     auth_token = await FastJWT().decode(request.headers["Authorisation"])
 
-    user: User = await User.find_one({"email": auth_token["email"]})
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
+#     user: User = await User.find_one({"email": auth_token["email"]})
+#     if not user:
+#         raise HTTPException(status_code=404, detail="User not found")
     
-    user.username = new_username.username
-    await user.save()
+#     user.username = new_username.username
+#     await user.save()
 
-    return {"message": "Username updated"}
+#     return {"message": "Username updated"}
 
 
 @profile_router.put("/")
