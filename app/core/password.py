58b2f84eebd83ef7dadbd20d736b834576662fd8
Ihor Savenko <csigorek@gmail.com>
2026-02-21 19:43:35 +0000

    temporary disable hashing

--- app/core/password.py
+++ app/core/password.py
@@ -4,15 +4,11 @@ import bcrypt
 
 from app.core.config import settings
 
+
 def hash_password(password: str) -> str:
-    """
-    Returns a salted password hash using a unique, 
-    automatically generated salt.
-    """
-    pwd_bytes = password.encode('utf-8')
-    hashed = bcrypt.hashpw(pwd_bytes, bcrypt.gensalt(rounds=12))
+    """Returns a salted password hash"""
     
-    return hashed.decode('utf-8')
+    return password
 
 def generate_password(length: int = 10) -> str:
     """Returns a random string of length"""
