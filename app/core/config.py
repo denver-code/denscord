733820a972a27e2d0be003d9219665c436451c7f
Ihor Savenko <csigorek@gmail.com>
2026-02-21 19:14:03 +0000

    change expose port adn remove cors validation

--- app/core/config.py
+++ app/core/config.py
@@ -12,15 +12,7 @@ class Settings(BaseSettings):
     SALT_SECRET_KEY: str
     JWT_SECRET_KEY: str
 
-    @validator("BACKEND_CORS_ORIGINS", pre=True)
-    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
-        if isinstance(v, str) and not v.startswith("["):
-            return [i.strip() for i in v.split(",")]
-        elif isinstance(v, (list, str)):
-            return v
-        raise ValueError(v)
 
-    
 
     class Config:
         case_sensitive = True
