ff127dc7727aa09722ed2afc29b21e51e4f367b0
Ihor Savenko <csigorek@gmail.com>
2026-02-21 19:18:49 +0000

    change cors typing

--- app/core/config.py
+++ app/core/config.py
@@ -7,7 +7,7 @@ from pydantic import AnyHttpUrl, BaseSettings, validator
 
 class Settings(BaseSettings):
     PROJECT_NAME: str
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+    BACKEND_CORS_ORIGINS: List[str] = []
     DATABASE_URL: str
     SALT_SECRET_KEY: str
     JWT_SECRET_KEY: str
