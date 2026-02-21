0edb68b7e346ad267417eb8472718e1759a38a04
Ihor Savenko <csigorek@gmail.com>
2026-02-21 19:38:48 +0000

    auto generate salt

--- app/core/password.py
+++ app/core/password.py
@@ -4,11 +4,15 @@ import bcrypt
 
 from app.core.config import settings
 
-
 def hash_password(password: str) -> str:
-    """Returns a salted password hash"""
+    """
+    Returns a salted password hash using a unique, 
+    automatically generated salt.
+    """
+    pwd_bytes = password.encode('utf-8')
+    hashed = bcrypt.hashpw(pwd_bytes, bcrypt.gensalt(rounds=12))
     
-    return bcrypt.hashpw(password.encode(), settings.SALT_SECRET_KEY.encode()).decode()
+    return hashed.decode('utf-8')
 
 def generate_password(length: int = 10) -> str:
     """Returns a random string of length"""
